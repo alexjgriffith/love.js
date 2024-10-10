67b56704080c0c7e47121592b4f772b7d03b7b86
alexjgriffith <griffitaj@gmail.com>
2024-10-10 19:41:19 -0400

    Working with emscripten 3.1.68

--- sample/update-sample.sh
+++ sample/update-sample.sh
@@ -2,4 +2,4 @@ gamefile=$1
 size=$(du -b $gamefile | awk '{print $1}')
 ln -s $(pwd)/$gamefile $(pwd)/release/$gamefile
 ln -s $(pwd)/$gamefile $(pwd)/compat/$gamefile 
-cat index.html.in | sed "s/{{{size}}}/${size}/g" | sed "s/{{{gamefile}}}/${gamefile}/g" > index.html
+cat index.html.in | sed "s/{{{size}}}/$size/g" | sed "s/{{{gamefile}}}/$gamefile/g" > index.html
